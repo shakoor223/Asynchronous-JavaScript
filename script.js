70593429afbff0bfe8ac196f7b1c604ee2ed77ed
shakoor223 <shakirahpadder490@gmail.com>
2024-12-23 08:07:41 +0530

    complete chaning promise

--- script.js
+++ script.js
@@ -27,37 +27,72 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1;
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  // Here we send of the request  Then that request  fetches the data in background and once that is done  it will emit the load event
-  request.send();
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country 1;
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   // Here we send of the request  Then that request  fetches the data in background and once that is done  it will emit the load event
+//   request.send();
 
-  request.addEventListener('load', function () {
-    // Converting Json data into an object
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     // Converting Json data into an object
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    // Render country
-    renderCountry(data);
-    // Get neighbour country 2
-    const [neighbour] = data.borders;
+//     // Render country
+//     renderCountry(data);
+//     // Get neighbour country 2
+//     const [neighbour] = data.borders;
 
-    // AJAX call 2
-    if (!neighbour) return;
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
-    request2.send();
+//     // AJAX call 2
+//     if (!neighbour) return;
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
 
-      renderCountry(data2, 'neighbour');
-    });
-  });
-};
-getCountryAndNeighbour('usa');
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+// getCountryAndNeighbour('usa');
 // getCountryData('saudi');
 // getCountryData('usa');
+
+// Promise And Fetch API
+// const request = fetch('https://restcountries.com/v2/name/portgual');
+// console.log(request);
+
+/////////////////////////////////////////
+////CONSUMING PROMISES/////////
+//////////////////////////////////////////
+const getCountryData = function (country) {
+  // calling the fetch func like this will then immediately return a promise so as soon as we start the req.
+  // In beginning this promise is still pending because the ASYNDHRONOUS task of getting the data is still running in the background.
+  // TO HANDEL THE FULLFILLED STATE WE CAN USE THE then method that is available on all promises.
+  // In the CALL BACK FUN in THEN Method we Name the Argumet response because this is the Response of an AJAX Call
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+      // The JSON Method here is a Mehtod that is available on all the  response objects that is coming from the fetch function.
+      // Now the Problem here is that this JSON fun itself is actually also an ASYC function... It will also return a new promise.
+      // We need to return this promise from here because this also returns a promise.. We also need to handle that promise as well
+      return response.json();
+    })
+
+    .then(function (data) {
+      console.log(data);
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+      //By returning this promise here then the fullfilled value of the next then method will be fullfilled value of this previous promise
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+getCountryData('iran');
